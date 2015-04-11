5de136c9233ed310a9dc27cd297b8dec1a843e5f
HuQiang <yinuo2015@gmail.com>
2015-04-12 01:18:23 +0800

    Initial Commit

---
+++ Lottery/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lottery
+//
+//  Created by 胡强 on 4/12/15.
+//  Copyright (c) 2015 huqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
